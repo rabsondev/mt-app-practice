7415c82158c24d82da09c31e3d5453259166601c
rabsondev <rabson@ostorlab.dev>
2024-06-06 08:20:17 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul>
+        <li v-for="catgeory in triviaCategories" :key="catgeory.id">
+            {{ catgeory.name }}
+        </li>
+    </ul>
+</template>
+
+<script setup lang="ts">
+const triviaCategories = ref([])
+const { data: categories } = useFetch('https://opentdb.com/api_category.php')
+triviaCategories.value = categories?.value?.trivia_categories || []
+</script>
